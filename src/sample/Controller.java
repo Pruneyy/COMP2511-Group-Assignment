4ec900253ef1f77ca085a66bedc9d49cb06f79d6
Chris Armstrong <chrisjarmstrong97@gmail.com>
2018-05-07 01:15:56 +1000

    Modified layout to use a BorderPane w/ GridPane in center and controls on right. Replaced Text place holders with coloured rectangle placeholders. Resized window.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,12 +4,15 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class Controller {
-	@FXML private GridPane game;
+	@FXML private BorderPane game;
 	@FXML private Button start;
+	@FXML private GridPane gameBoard;
 	private boolean startFlag = false;
 	
 	@FXML protected void handleButtonPress(ActionEvent event) {
@@ -26,7 +29,11 @@ public class Controller {
 	public void makeGrid() {
 		for(int i = 0; i < 6; i++) {
 			for(int j = 0; j < 6; j++) {
-				game.add(new Text("Wow"), i, j);
+				Rectangle rec = new Rectangle();
+				rec.setWidth(50);
+				rec.setHeight(50);
+				if ((i%2 == 0 && j%2 == 0) || (i%2 != 0 && j%2 != 0)) rec.setFill(Color.AQUAMARINE);
+				gameBoard.add(rec, i, j);
 			}
 		}
 	}
