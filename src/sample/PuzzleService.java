31864c32e1eaa70665fd0d95bc183fffb33c4770
Dong <dong@helixta.com.au>
2018-05-24 22:43:03 +1000

    Revert "Added difficulty settings and early implementation of difficulty map generation"
    This reverts commit c257a156ddd0a161179f0f409748f969926cb8c1 - it removes a feature

--- src/sample/PuzzleService.java
+++ src/sample/PuzzleService.java
@@ -1,15 +1,13 @@
 package sample;
 
 /**
- * Used to set the difficulty of the game that will be passed to the puzzle generator.
- * enum allows for more flexibility in managing difficulties.
+ * A Service to supply puzzles
  * @author dong
  */
 public interface PuzzleService {
     enum Difficulty {
-        EASY, MEDIUM, HARD, DEMO
+        EASY, MEDIUM, HARD
     };
-
     /** Returns a new puzzle to be solved, in the form of a Grid with cars already populated */
     Grid getNewPuzzle(Difficulty d);
 }
