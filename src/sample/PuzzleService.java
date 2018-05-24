c257a156ddd0a161179f0f409748f969926cb8c1
Utkarsh Sood <usood.unsw@gmail.com>
2018-05-24 21:49:09 +1000

    Added difficulty settings and early implementation of difficulty map generation

--- src/sample/PuzzleService.java
+++ src/sample/PuzzleService.java
@@ -1,13 +1,15 @@
 package sample;
 
 /**
- * A Service to supply puzzles
+ * Used to set the difficulty of the game that will be passed to the puzzle generator.
+ * enum allows for more flexibility in managing difficulties.
  * @author dong
  */
 public interface PuzzleService {
     enum Difficulty {
-        EASY, MEDIUM, HARD
+        EASY, MEDIUM, HARD, DEMO
     };
+
     /** Returns a new puzzle to be solved, in the form of a Grid with cars already populated */
     Grid getNewPuzzle(Difficulty d);
 }
--- src/sample/SplashScreenController.java
+++ src/sample/SplashScreenController.java
@@ -1,52 +1,64 @@
 package sample;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.animation.FadeTransition;
 import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
-import javafx.event.ActionEvent;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import sample.Main.Theme;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
+
+/**
+ * This class handles the JavaFX splash screens which are animated during UI interactions.
+ */
 public class SplashScreenController implements Initializable {
 
 	private static Double FADE_TIME = 0.5;		// the fade timer
-	
-	@FXML private AnchorPane navBar;
+	private static Integer NAVBAR_THEME_HIDE = -150;		// x value to hide the theme navigation bar
+	private static Integer NAVBAR_THEME_SHOW = 0;			// x value to show the theme navigation bar
+	private static Integer NAVBAR_DIFF_HIDE = 670;			// x value to hide the difficulty navigation bar
+	private static Integer NAVBAR_DIFF_SHOW = 370;			// x value to show the difficulty navigation bar
+
+	@FXML private AnchorPane navBarTheme;
+	@FXML private AnchorPane navBarDiff;
 	@FXML private AnchorPane rootPane;
 	@FXML private Text title;
 	@FXML private Text instructions;
 	@FXML private Button start;
+	@FXML private Button difficulties;
 	@FXML private Button themes;
 	@FXML private Button quit;
 	@FXML private ImageView background;
 	@FXML private ToggleGroup theme;
-	@FXML private RadioButton carThemeRadioButton;
-	@FXML private RadioButton planeThemeRadioButton;
-	@FXML private RadioButton animalThemeRadioButton;
+	@FXML private ToggleGroup difficulty;
 
+	/**
+	 * This will initialise all the
+	 * @param url
+	 * @param rb
+	 */
 	@Override
 	public void initialize (URL url, ResourceBundle rb) {
 
 		FadeTransition fadeIn = Controller.fadeSet(title, FADE_TIME + 1, 0, 1, 1);
 		FadeTransition fadeButtonOne = Controller.fadeSet(start, FADE_TIME, 0, 1, 1);
-		FadeTransition fadeButtonTwo = Controller.fadeSet(themes, FADE_TIME, 0, 1, 1);
-		FadeTransition fadeButtonThree = Controller.fadeSet(quit, FADE_TIME, 0, 1, 1);
+		FadeTransition fadeButtonTwo = Controller.fadeSet(difficulties, FADE_TIME, 0, 1, 1);
+		FadeTransition fadeButtonThree = Controller.fadeSet(themes, FADE_TIME, 0, 1, 1);
+		FadeTransition fadeButtonFour = Controller.fadeSet(quit, FADE_TIME, 0, 1, 1);
 		FadeTransition fadeIns = Controller.fadeSet(instructions, FADE_TIME, 0, 1, 1);
 
 		fadeIn.play();
@@ -62,19 +74,16 @@ public class SplashScreenController implements Initializable {
 		fadeButtonTwo.setOnFinished((e)->{
 			fadeButtonThree.play();
 		});
-		
+
 		fadeButtonThree.setOnFinished((e)->{
+			fadeButtonFour.play();
+		});
+
+		fadeButtonFour.setOnFinished((e)->{
 			fadeIns.play();
 		});
-		
-		slideMenuLeft();
-		if (Main.currentTheme == Theme.CARS) {
-			carThemeRadioButton.fire();
-		} else if (Main.currentTheme == Theme.PLANE) {
-			planeThemeRadioButton.fire();
-		} else if (Main.currentTheme == Theme.ANIMALS) {
-			animalThemeRadioButton.fire();
-		}
+
+		slideMenu();
 	}
 
 	@FXML protected void handleStartGamePress(ActionEvent event) {
@@ -86,7 +95,11 @@ public class SplashScreenController implements Initializable {
 			System.out.println("feels bad mang");
 		}
 	}
-	
+
+	@FXML protected void handleDifficultiesPress(ActionEvent event) {
+		System.out.println("Cool story bro");
+	}
+
 	@FXML protected void handleQuitGamePress(ActionEvent event) {
 		System.exit(0);
 	}
@@ -105,7 +118,19 @@ public class SplashScreenController implements Initializable {
 		Main.currentTheme = Theme.ANIMALS;
 		setViewModel("Images/AnimalSplash.jpg");
 	}
-	
+
+	@FXML protected void handleEasySelect(ActionEvent event) {
+		Controller.currentDifficulty = PuzzleService.Difficulty.EASY;
+	}
+
+	@FXML protected void handleMediumSelect(ActionEvent event) {
+		Controller.currentDifficulty = PuzzleService.Difficulty.MEDIUM;
+	}
+
+	@FXML protected void handleHardSelect(ActionEvent event) {
+		Controller.currentDifficulty = PuzzleService.Difficulty.HARD;
+	}
+
 	public FXMLLoader loadView(String fxmlPath) {
 		return Controller.genericViewLoader(fxmlPath, rootPane);
 	}
@@ -114,17 +139,37 @@ public class SplashScreenController implements Initializable {
 		background.setImage(new Image(backgroundSrc));
 	}
 	
-    private void slideMenuLeft() {
-        TranslateTransition openNav=new TranslateTransition(new Duration(350), navBar);
-        openNav.setToX(0);
-        TranslateTransition closeNav=new TranslateTransition(new Duration(350), navBar);
-        themes.setOnAction((ActionEvent evt)->{
-            if(navBar.getTranslateX()!=0){
-                openNav.play();
+    private void slideMenu() {
+        TranslateTransition openNavTheme=new TranslateTransition(new Duration(350), navBarTheme);
+		TranslateTransition openNavDiff=new TranslateTransition(new Duration(350), navBarDiff);
+		openNavTheme.setToX(NAVBAR_THEME_SHOW);
+		openNavDiff.setToX(NAVBAR_DIFF_SHOW);
+
+		TranslateTransition closeNavTheme=new TranslateTransition(new Duration(350), navBarTheme);
+		TranslateTransition closeNavDiff=new TranslateTransition(new Duration(350), navBarDiff);
+
+		themes.setOnAction((ActionEvent evt)->{
+            if(navBarTheme.getTranslateX()!=NAVBAR_THEME_SHOW){
+                openNavTheme.play();
+				closeMenu(closeNavDiff, NAVBAR_DIFF_HIDE);
             }else{
-                closeNav.setToX(-(navBar.getWidth()));
-                closeNav.play();
+            	closeMenu(closeNavTheme, NAVBAR_THEME_HIDE);
             }
         });
+
+		difficulties.setOnAction((ActionEvent evt)->{
+			if(navBarDiff.getTranslateX()!=NAVBAR_DIFF_SHOW){
+				openNavDiff.play();
+				closeMenu(closeNavTheme, NAVBAR_THEME_HIDE);
+			}else{
+				closeMenu(closeNavDiff, NAVBAR_DIFF_HIDE);
+			}
+		});
     }
+
+	private void closeMenu(TranslateTransition closeNav, Integer xValue) {
+		closeNav.setToX(xValue);
+		closeNav.play();
+	}
+
 }
